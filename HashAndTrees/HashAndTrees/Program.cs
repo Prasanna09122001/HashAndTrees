6075fd30e324a8af959e00a44e3b2764494b0c7e
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-31 17:41:58 +0530

    [Prasanna] Add: implement the Hash map function

--- HashAndTrees/HashAndTrees/Program.cs
+++ HashAndTrees/HashAndTrees/Program.cs
@@ -5,7 +5,16 @@ namespace HashAndTrees
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to the Hash And Trees Problem");
+            Console.WriteLine("Write a Statement");
+            string statement = Console.ReadLine();
+            string[] sentence = statement.Split(" ");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(sentence.Length);
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                hash.Add(Convert.ToString(i), sentence[i]);
+            }
+            string hash1= hash.get("5");
+            Console.WriteLine("The index Value: "+hash1);
         }
     }
 }
