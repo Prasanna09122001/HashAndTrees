49f4b842d05c6cfb728fa89d5700bdd0e77d782d
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-31 12:08:15 +0530

    [Prasanna] Add: Structure Folder Created

---
+++ HashAndTrees/HashAndTrees/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HashAndTrees
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Hash And Trees Problem");
+        }
+    }
+}
