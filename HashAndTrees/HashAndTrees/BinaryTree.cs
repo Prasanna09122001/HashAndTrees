70f7b13d48d33d545586825d34a6143061c1fb80
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-08-01 17:40:05 +0530

    [Prasanna] Add: Implement the Binary Search Tree

--- HashAndTrees/HashAndTrees/BinaryTree.cs
+++ HashAndTrees/HashAndTrees/BinaryTree.cs
@@ -1,17 +1,18 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace HashAndTrees
 {
-    internal class BinaryTree<T> where T: IComparable<T>
+    internal class BinarySearchTree<T> where T: IComparable<T>
     {
         public T NodeData { get; set; }
-        public BinaryTree<T> LeftTree { get; set; }
-        public BinaryTree<T> RightTree { get; set; }
-        public BinaryTree(T nodeData)
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T nodeData)
         {
             this.NodeData = nodeData;
             this.LeftTree = null;
@@ -22,9 +23,35 @@ namespace HashAndTrees
         public void Insert(T item)
         {
             T currentNodeValue = this.NodeData;
-            if(currentNodeValue.CompareTo(item)=0)
-            { }
+            if((currentNodeValue.CompareTo(item))>0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {  
+            if (this.LeftTree != null)
+            {
+                this.leftcount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightcount++;
+                this.RightTree.Display();
+
+            }
         }
     }
 }
-*/
